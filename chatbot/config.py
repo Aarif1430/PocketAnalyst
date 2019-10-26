ad1ec546871b6f38deb1d2132dd81c8aa88f7829
Matt Katz <mykatz01@gmail.com>
2019-10-26 03:57:07 -0700

    integration w/ fb

--- chatbot/config.py
+++ chatbot/config.py
@@ -6,3 +6,6 @@ PROTECTED_INTENTS = set([
     "account.create.q2",
     "get-stock-info"
 ])
+
+FB_ACCESS_TOKEN = "EAAkZBBLkY9IUBAG39yqid0oAb7xkOPW9mu0MzgvxSnTOaLL8f5AyFf6nCAozoYzfWgQLA4RgtZCtRDbpxJH9e7T3x2M5nV8PFeRJxfsI7l9NrCRDJo45GRVt5mu50jxAgz3Fg8P5qy41TiuL107c6c6VlRLMg8bZCwPvlxZAVpc7jovR5i37"
+FB_VERIFY_TOKEN = "test"
